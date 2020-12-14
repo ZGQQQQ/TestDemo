5f0f0a8b8cdedb52a6a7e78102b988c7dea056da
ZGQQQQ <1731073069@qq.com>
2020-12-15 05:31:33 +0800

    测试。。。。。。

--- MoreThread/src/main/java/com/Demo/noUseThread/Person.java
+++ MoreThread/src/main/java/com/Demo/noUseThread/Person.java
@@ -6,7 +6,7 @@ public class Person {
 	Person(String name) {
 		this.name = name;
 	}
-
+//666666666666666666666666666666
 	public void show() {
 		for (int i = 0; i < 10; i++) {
 			System.out.println(name + "---" + Thread.currentThread().getName() + "---" + i);
