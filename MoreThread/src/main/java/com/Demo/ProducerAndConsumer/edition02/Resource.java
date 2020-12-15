9953db97ef219d6e631abb46ab6f974a21814a7d
zhuguangquan <zhuguangquan@enmonster.com>
2020-12-15 16:49:05 +0800

    测试lock

--- MoreThread/src/main/java/com/Demo/ProducerAndConsumer/edition02/Resource.java
+++ MoreThread/src/main/java/com/Demo/ProducerAndConsumer/edition02/Resource.java
@@ -4,27 +4,30 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-/**资源类*/
+/**
+ * 资源类
+ */
 public class Resource {
 	private String name;
 	private int count = 1;
 	private boolean flag = true;
 
 	Lock lock = new ReentrantLock();//创建锁
-	Condition producer_lock = lock.newCondition();
-	Condition consumer_lock = lock.newCondition();
+	Condition producer_con = lock.newCondition();
+	Condition consumer_con = lock.newCondition();
 
 	//给生产者使用的方法
 	public void set(String name) {
 		lock.lock();
 		try {
 			while (flag)
-				try {producer_lock.await();} catch (InterruptedException e) {}
+				try {
+					producer_con.await();} catch (InterruptedException e) {}
 			this.name = name + count;
 			count++;
 			System.out.println(Thread.currentThread().getName() + "生成了第" + count + "只烤鸭" + "。。。");
 			flag = true;
-			consumer_lock.signal();
+			consumer_con.signal();
 		} finally {
 			lock.unlock();
 		}
@@ -36,10 +39,11 @@ public class Resource {
 		lock.lock();
 		try {
 			while (!flag)
-				try {consumer_lock.await();} catch (InterruptedException e) {}
+				try {
+					consumer_con.await();} catch (InterruptedException e) {}
 			System.out.println(Thread.currentThread().getName() + "消费了第" + count + "只烤鸭");
 			flag = false;
-			producer_lock.signal();
+			producer_con.signal();
 		} finally {
 			lock.unlock();
 		}
